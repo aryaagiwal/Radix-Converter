1621688b70da60ae5626c98af848382d434f0ca7
aryaagiwal <aryagiwal@gmail.com>
2022-02-25 16:44:08 -0600

    changed it so that accuracy of fractionals is adjustable

--- converter.c
+++ converter.c
@@ -2,75 +2,49 @@
 #include <stdlib.h>
 #include <math.h>
 
-int * convert_to_decimal(int number, int frac, int radix) {
+int * convert_to_decimal(int number, int frac, int radix, int dec) {
     static int num[2];
     num[0] = 0;
     int factor = 0;
-    printf("%d and %d",number,frac);
     while (number > 0) {
         num[0] += (number%10 * pow(radix,factor));
         factor++;
         number /= 10;
     }
-    while (frac < 9999) {
+    while (frac < (int) pow(10,dec-1) - 1) {
         frac *= 10;
     }
     num[1] = 0;
-    num[1] += (frac % 10) * pow(radix, -5) * pow(10,5);
-    frac /= 10;
-    frac *= 10;
-    num[1] += (frac % 100) * pow(radix, -4) * pow(10,4);
-    frac /= 100;
-    frac *= 100;
-    num[1] += (frac % 1000) * pow(radix, -3) * pow(10,3);
-    frac /= 1000;
-    frac *= 1000;
-    num[1] += (frac % 10000) * pow(radix, -2) * pow(10,2);
-    frac /= 10000;
-    frac *= 10000;
-    num[1] += (frac % 100000) * pow(radix, -1) * pow(10,1); 
+    for (int i = dec; i > 0; i--) {
+        num[1] += (frac % ((int) pow(10,dec+1-i))) * pow(radix, -i) * pow(10,i);
+        frac /= pow(10,dec+1-i);
+        frac *= pow(10,dec+1-i);
+    }
     return num;
 }
 
-int * convert_dec_to_n(int number[2], int radix) {
-    int whole_portion = number[0];
-    int frac_portion = number[1];
+int * convert_dec_to_n(int num[2], int radix, int dec) {
+    //printf("%d",num[1]);
+    int frac_portion = num[1];
     int new_whole = 0;
     int mult_factor = 1;
-    while (whole_portion/radix > 0) {
-        new_whole += (whole_portion%radix) * mult_factor;
+    while (num[0]/radix > 0) {
+        new_whole += (num[0]%radix) * mult_factor;
         mult_factor *= 10;
-        whole_portion /= radix;
+        num[0] /= radix;
     }
-    new_whole += whole_portion * mult_factor;
+    new_whole += num[0] * mult_factor;
     //whole portion done
     //now have to find fractional portion
     int new_frac = 0;
-    static int num[2];
     num[0] = new_whole;
     num[1] = 0;
-    new_frac = (frac_portion % 100000) / ((pow(radix, -1) * pow(10,5)));
-    new_frac *= pow(10,4);
-    num[1] += new_frac;
-    frac_portion = frac_portion % (int) ((pow(radix, -1) * pow(10,5)));
-
-    new_frac = (frac_portion % 10000) / ((pow(radix, -1) * pow(10,4)));
-    new_frac *= pow(10,3);
-    num[1] += new_frac;
-    frac_portion = frac_portion % (int) ((pow(radix, -1) * pow(10,4)));
-
-    new_frac = (frac_portion % 1000) / ((pow(radix, -1) * pow(10,3)));
-    new_frac *= pow(10,2);
-    num[1] += new_frac;
-    frac_portion = frac_portion % (int) ((pow(radix, -1) * pow(10,3)));
-
-    new_frac = (frac_portion % 100) / ((pow(radix, -1) * pow(10,2)));
-    new_frac *= pow(10,1);
-    num[1] += new_frac;
-    frac_portion = frac_portion % (int) ((pow(radix, -1) * pow(10,2)));
-
-    num[1] += (frac_portion % 10) / ((pow(radix, -1) * pow(10,1)));
-    
+    for (int i = dec; i > 0; i--) {
+        new_frac = (frac_portion % ((int) pow(10,i))) / ((pow(radix, -1) * pow(10,i)));
+        new_frac *= pow(10,i-1);
+        num[1] += new_frac;
+        frac_portion = frac_portion % (int) ((pow(radix, -1) * pow(10,i)));
+    }
     printf("\n%d.%d\n\n",num[0],num[1]);
     return num;
 }
@@ -90,14 +64,17 @@ int main() {
             run = 0;
             break;
         }
+        printf("How many decimal places would you like? \n");
+        int dec = 0;
+        scanf("%d",&dec);
         printf("Enter a base:\n");
         int base = 0;
         scanf("%d", &base);
-        int *num = convert_to_decimal(whole, frac, base);
+        int *num = convert_to_decimal(whole, frac, base, dec);
         printf("Enter a base to convert to:\n");
         int new_base = 10;
         scanf("%d",&new_base);
-        convert_dec_to_n(num, new_base);
+        convert_dec_to_n(num, new_base, dec);
     }
     return EXIT_SUCCESS;
 }
